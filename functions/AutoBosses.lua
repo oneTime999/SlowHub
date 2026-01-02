4f2ff9316437aa7b2fc349f031db98767eb33c81
oneTime999 <onetime.zkk@gmail.com>
2026-01-02 20:21:14 -0300

    Update AutoBosses.lua

--- functions/AutoBosses.lua
+++ functions/AutoBosses.lua
@@ -1,4 +1,4 @@
-local Tab = _G.MiscTab
+local Tab = _G.BossesTab
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
