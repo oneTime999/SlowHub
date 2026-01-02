268d0fce2362eed77aaa9f13eabf2690915e7218
oneTime999 <onetime.zkk@gmail.com>
2026-01-02 19:21:47 -0300

    Delete functions/bosses.lua

--- functions/bosses.lua
+++
@@ -1,8 +0,0 @@
-local Tab = _G.BossesTab
-local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
-
--- Seção: Boss Farm
-Tab:CreateSection("Boss Farm")
-
--- Carregar Auto Bosses
-loadstring(game:HttpGet(githubBase .. "AutoBosses.lua"))()
