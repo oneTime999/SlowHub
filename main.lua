5e78da911f71f663173b6931869e321d425569a8
oneTime999 <onetime.zkk@gmail.com>
2026-01-02 18:27:26 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,40 +1,44 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 -- Variáveis globais
 _G.SlowHub = {
     AutoFarmLevel = false,
     AutoFarmBoss = false,
-    AutoHaki = false
+    AutoHaki = false,
+    SelectedWeapon = nil
 }
 
 -- Criar janela
-local Window = OrionLib:MakeWindow({
+local Window = Rayfield:CreateWindow({
     Name = "Slow Hub",
-    HidePremium = false,
-    SaveConfig = true,
-    ConfigFolder = "SlowHub"
+    LoadingTitle = "Slow Hub",
+    LoadingSubtitle = "by oneTime999",
+    ConfigurationSaving = {
+        Enabled = true,
+        FolderName = "SlowHub",
+        FileName = "SlowHubConfig"
+    },
+    Discord = {
+        Enabled = false,
+        Invite = "noinvitelink",
+        RememberJoins = true
+    },
+    KeySystem = false
 })
 
 -- Criar Tab Main e salvar globalmente
-_G.MainTab = Window:MakeTab({
-    Name = "Main",
-    Icon = "rbxassetid://4483345998",
-    PremiumOnly = false
-})
+_G.MainTab = Window:CreateTab("Main", 4483345998)
 
--- Salvar OrionLib globalmente para notificações
-_G.OrionLib = OrionLib
+-- Salvar Rayfield globalmente para notificações
+_G.Rayfield = Rayfield
 
 -- Carregar Tab Main
 loadstring(game:HttpGet("https://raw.githubusercontent.com/oneTime999/SlowHub/main/tabs/main.lua"))()
 
--- Inicializar
-OrionLib:Init()
-
--- Notificação
-OrionLib:MakeNotification({
-    Name = "Slow Hub",
+-- Notificação de boas-vindas
+Rayfield:Notify({
+    Title = "Slow Hub",
     Content = "Carregado com sucesso!",
-    Image = "rbxassetid://4483345998",
-    Time = 5
+    Duration = 5,
+    Image = 4483345998
 })
