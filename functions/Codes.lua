8866ef76affb939dd84cf5333e9024372e1859d6
oneTime999 <onetime.zkk@gmail.com>
2026-01-02 20:01:29 -0300

    Create Codes.lua

---
+++ functions/Codes.lua
@@ -0,0 +1,30 @@
+local Tab = _G.MiscTab
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local codes = {
+    "RELEASE",
+    "UPDATE0.5",
+    "TRAITS",
+    "NEWYEAR",
+    "CHRISTMAS",
+    "200KVISITS",
+    "3000CCU",
+    "3KLIKES",
+    "QUESTBUGFIXSORRY"
+}
+
+local function redeemAllCodes()
+    for _, code in ipairs(codes) do
+        pcall(function()
+            ReplicatedStorage.RemoteEvents.CodeRedeem:InvokeServer(code)
+        end)
+        wait(0.5)
+    end
+end
+
+Tab:CreateButton({
+    Name = "Redeem All Codes",
+    Callback = function()
+        redeemAllCodes()
+    end
+})
