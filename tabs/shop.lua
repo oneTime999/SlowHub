cf448a4e965e890cfe7012d4eac60d0e614d6185
oneTime999 <onetime.zkk@gmail.com>
2026-01-03 04:13:59 -0300

    Add Meele section and load corresponding script

--- tabs/shop.lua
+++ tabs/shop.lua
@@ -4,3 +4,7 @@ local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/fu
 Tab:CreateSection("Sword")
 
 loadstring(game:HttpGet(githubBase .. "Swords.lua"))()
+
+Tab:CreateSection("Meele")
+
+loadstring(game:HttpGet(githubBase .. "Meele.lua"))()
