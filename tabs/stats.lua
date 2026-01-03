04c207ba30950a4e51a381911737b3bbeb1ec3eb
oneTime999 <onetime.zkk@gmail.com>
2026-01-03 04:27:43 -0300

    Add Stats section and load Stats.lua from GitHub

--- tabs/stats.lua
+++ tabs/stats.lua
@@ -1,1 +1,6 @@
+local Tab = _G.ShopTab
+local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
+Tab:CreateSection("Stats")
+
+loadstring(game:HttpGet(githubBase .. "Stats.lua"))()
