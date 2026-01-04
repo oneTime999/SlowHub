e1f113b6e47b9fe2553c099ddf520c19d2494b7e
oneTime999 <onetime.zkk@gmail.com>
2026-01-04 16:59:03 -0300

    Update bosses.lua

--- tabs/bosses.lua
+++ tabs/bosses.lua
@@ -4,3 +4,7 @@ local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/fu
 Tab:CreateSection("Boss Farm")
 
 loadstring(game:HttpGet(githubBase .. "AutoBosses.lua"))()
+
+Tab:CreateSection("Mini Boss Farm")
+
+loadstring(game:HttpGet(githubBase .. "MiniBossFarm.lua"))()
