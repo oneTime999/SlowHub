80ba32ab1a108dd13df64cec3d10fadd1251fd11
oneTime999 <onetime.zkk@gmail.com>
2026-01-04 03:09:47 -0300

    Create AntiAFK.lua

---
+++ functions/AntiAFK.lua
@@ -0,0 +1,71 @@
+local Tab = _G.MiscTab
+local Players = game:GetService("Players")
+local VirtualUser = game:GetService("VirtualUser")
+local RunService = game:GetService("RunService")
+local Player = Players.LocalPlayer
+
+local antiAfkConnection = nil
+local idledConnection = nil
+
+local function stopAntiAfk()
+    if antiAfkConnection then
+        antiAfkConnection:Disconnect()
+        antiAfkConnection = nil
+    end
+    
+    if idledConnection then
+        idledConnection:Disconnect()
+        idledConnection = nil
+    end
+    
+    _G.SlowHub.AntiAfk = false
+end
+
+local function startAntiAfk()
+    if antiAfkConnection or idledConnection then
+        stopAntiAfk()
+    end
+
+    _G.SlowHub.AntiAfk = true
+
+    idledConnection = Player.Idled:Connect(function()
+        if not _G.SlowHub.AntiAfk then
+            stopAntiAfk()
+            return
+        end
+        
+        VirtualUser:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+        task.wait(1)
+        VirtualUser:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    end)
+
+    antiAfkConnection = RunService.Heartbeat:Connect(function()
+        if not _G.SlowHub.AntiAfk then
+            stopAntiAfk()
+            return
+        end
+    end)
+end
+
+Tab:CreateToggle({
+    Name = "Anti AFK",
+    CurrentValue = _G.SlowHub.AntiAfk,
+    Flag = "AntiAfkToggle",
+    Callback = function(Value)
+        if Value then
+            startAntiAfk()
+        else
+            stopAntiAfk()
+        end
+        
+        _G.SlowHub.AntiAfk = Value
+        if _G.SaveConfig then
+            _G.SaveConfig()
+        end
+    end
+})
+
+if _G.SlowHub.AntiAfk then
+    task.wait(2)
+    startAntiAfk()
+end
