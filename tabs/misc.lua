3de125424c9b12df2499c52627d5c2869d2c468d
oneTime999 <onetime.zkk@gmail.com>
2026-01-03 21:45:46 -0300

    Update misc.lua

--- tabs/misc.lua
+++ tabs/misc.lua
@@ -1,14 +1,10 @@
 local Tab = _G.MiscTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
--- Secao Haki
 Tab:CreateSection("Haki")
 
--- Carregar Auto Haki
 loadstring(game:HttpGet(githubBase .. "AutoHaki.lua"))()
 
--- Secao Codes
 Tab:CreateSection("Codes")
 
--- Carregar Reedem All Codes
 loadstring(game:HttpGet(githubBase .. "Codes.lua"))()
