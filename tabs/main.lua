5fab0399f9f672d197e949fe0dea51de38412f2e
oneTime999 <onetime.zkk@gmail.com>
2026-01-03 21:45:25 -0300

    Update main.lua

--- tabs/main.lua
+++ tabs/main.lua
@@ -1,14 +1,10 @@
 local Tab = _G.MainTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
--- Secao Configuracoes
 Tab:CreateSection("Settings")
 
--- Carregar Seletor de Armas
 loadstring(game:HttpGet(githubBase .. "WeaponSelector.lua"))()
 
--- Secao Auto Farm
 Tab:CreateSection("Auto Farm")
 
--- Carregar Auto Level
 loadstring(game:HttpGet(githubBase .. "AutoLevel.lua"))()
