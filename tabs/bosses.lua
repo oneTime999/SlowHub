ac2038dba7f01008b333ef49e3eea7e64c7c7891
oneTime999 <onetime.zkk@gmail.com>
2026-01-03 21:45:04 -0300

    Update bosses.lua

--- tabs/bosses.lua
+++ tabs/bosses.lua
@@ -1,8 +1,6 @@
 local Tab = _G.BossesTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
--- Secao Boss Farm
 Tab:CreateSection("Boss Farm")
 
--- Carregar Auto Bosses
 loadstring(game:HttpGet(githubBase .. "AutoBosses.lua"))()
