424e44a8c9dee6fd853df6028eae3a2536c86fef
oneTime999 <onetime.zkk@gmail.com>
2026-01-08 00:33:36 -0300

    Update shop.lua

--- tabs/shop.lua
+++ tabs/shop.lua
@@ -1,6 +1,10 @@
 local Tab = _G.ShopTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
+Tab:CreateSection("NPCs")
+
+loadstring(game:HttpGet(githubBase .. "NPC.lua"))()
+
 Tab:CreateSection("Sword")
 
 loadstring(game:HttpGet(githubBase .. "Swords.lua"))()
