ff200f14a9d3a70ac7800398320bce880c6fd1cb
oneTime999 <onetime.zkk@gmail.com>
2026-01-11 03:29:06 -0300

    Update stats.lua

--- tabs/stats.lua
+++ tabs/stats.lua
@@ -1,6 +1,10 @@
 local Tab = _G.StatsTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
-Tab:CreateSection("Stats")
+-- Stats Section
+Tab:AddParagraph({
+    Title = "Stats",
+    Content = ""
+})
 
 loadstring(game:HttpGet(githubBase .. "Stats.lua"))()
