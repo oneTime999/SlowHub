86dd3db2627cbb5e7ccf5a94c280efdb2d91dc83
oneTime999 <onetime.zkk@gmail.com>
2026-01-11 03:27:53 -0300

    Update misc.lua

--- tabs/misc.lua
+++ tabs/misc.lua
@@ -1,20 +1,35 @@
 local Tab = _G.MiscTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
-Tab:CreateSection("Hakis")
+-- Hakis Section
+Tab:AddParagraph({
+    Title = "Hakis",
+    Content = ""
+})
 
 loadstring(game:HttpGet(githubBase .. "AutoHaki.lua"))()
 loadstring(game:HttpGet(githubBase .. "AutoObservation.lua"))()
 
-
-Tab:CreateSection("Auto Skill")
+-- Auto Skill Section
+Tab:AddParagraph({
+    Title = "Auto Skill",
+    Content = ""
+})
 
 loadstring(game:HttpGet(githubBase .. "AutoSkill.lua"))()
 
-Tab:CreateSection("Codes")
+-- Codes Section
+Tab:AddParagraph({
+    Title = "Codes",
+    Content = ""
+})
 
 loadstring(game:HttpGet(githubBase .. "Codes.lua"))()
 
-Tab:CreateSection("AFK")
+-- AFK Section
+Tab:AddParagraph({
+    Title = "AFK",
+    Content = ""
+})
 
 loadstring(game:HttpGet(githubBase .. "AntiAFK.lua"))()
