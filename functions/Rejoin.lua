bde094f3efd5b594378eaac20b08b5f9f5f31ffd
oneTime999 <onetime.zkk@gmail.com>
2026-01-14 10:26:09 -0300

    Create Rejoin.lua

---
+++ functions/Rejoin.lua
@@ -0,0 +1,18 @@
+local Tab = _G.MiscTab
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+local Player = Players.LocalPlayer
+
+Tab:AddButton({
+    Title = "Rejoin Server",
+    Description = "Reconnects you to the current server",
+    Callback = function()
+        if #Players:GetPlayers() <= 1 then
+            Player:Kick("\nRejoining...")
+            task.wait()
+            TeleportService:Teleport(game.PlaceId, Player)
+        else
+            TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, Player)
+        end
+    end
+})
