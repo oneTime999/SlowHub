4204747ebd501d3f550b8f23f7a43610d49e532c
oneTime999 <onetime.zkk@gmail.com>
2026-01-26 13:52:05 -0300

    Update main.lua

--- tabs/main.lua
+++ tabs/main.lua
@@ -2,33 +2,21 @@ local Tab = _G.MainTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
 -- Settings Section
-Tab:AddParagraph({
-    Title = "Settings",
-    Content = ""
-})
+Tab:CreateLabel("Settings")
 
 loadstring(game:HttpGet(githubBase .. "WeaponSelector.lua"))()
 
 -- Auto Farm Section
-Tab:AddParagraph({
-    Title = "Auto Farm",
-    Content = ""
-})
+Tab:CreateLabel("Auto Farm")
 
 loadstring(game:HttpGet(githubBase .. "AutoLevel.lua"))()
 
 -- Auto Farm Selected Section
-Tab:AddParagraph({
-    Title = "Auto Farm Selected",
-    Content = ""
-})
+Tab:CreateLabel("Auto Farm Selected")
 
 loadstring(game:HttpGet(githubBase .. "AutoFarmSelectedMob.lua"))()
 
 -- Auto Dungeon Section
-Tab:AddParagraph({
-    Title = "Auto Dungeon",
-    Content = ""
-})
+Tab:CreateLabel("Auto Dungeon")
 
 loadstring(game:HttpGet(githubBase .. "AutoDungeon.lua"))()
