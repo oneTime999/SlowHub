d3f69bfc8a42b9c0cc407086def4857cabc95790
oneTime999 <onetime.zkk@gmail.com>
2026-01-26 13:51:51 -0300

    Update bosses.lua

--- tabs/bosses.lua
+++ tabs/bosses.lua
@@ -2,25 +2,16 @@ local Tab = _G.BossesTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
 -- Boss Farm Section
-Tab:AddParagraph({
-    Title = "Boss Farm",
-    Content = ""
-})
+Tab:CreateLabel("Boss Farm")
 
 loadstring(game:HttpGet(githubBase .. "AutoBosses.lua"))()
 
 -- Summon Section
-Tab:AddParagraph({
-    Title = "Summon",
-    Content = ""
-})
+Tab:CreateLabel("Summon")
 
 loadstring(game:HttpGet(githubBase .. "AutoSummon.lua"))()
 
 -- Mini Boss Farm Section
-Tab:AddParagraph({
-    Title = "Mini Boss Farm",
-    Content = ""
-})
+Tab:CreateLabel("Mini Boss Farm")
 
 loadstring(game:HttpGet(githubBase .. "MiniBossFarm.lua"))()
