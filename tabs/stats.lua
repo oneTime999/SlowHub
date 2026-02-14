6cc4d31372a97338afed3957997837dbe196fd9a
oneTime999 <onetime.zkk@gmail.com>
2026-02-14 20:00:07 -0300

    Delete tabs/stats.lua

--- tabs/stats.lua
+++
@@ -1,7 +0,0 @@
-local Tab = _G.StatsTab
-local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
-
--- Stats Section
-Tab:CreateLabel("Stats")
-
-loadstring(game:HttpGet(githubBase .. "Stats.lua"))()
