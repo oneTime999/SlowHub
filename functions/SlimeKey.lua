5bae80736f5e8dff3e3515df81a99391e1344ae7
oneTime999 <onetime.zkk@gmail.com>
2026-02-14 00:14:59 -0300

    Update SlimeKey.lua

--- functions/SlimeKey.lua
+++ functions/SlimeKey.lua
@@ -1,30 +1,61 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
 
-if not _G.SlowHub then _G.SlowHub = {} end
-_G.SlowHub.AutoCraftSlime = false
+local autoCraftConnection = nil
+local lastCraftTime = 0
 
-Tab:CreateSection("Crafting Settings")
+local function stopAutoCraft()
+    if autoCraftConnection then
+        autoCraftConnection:Disconnect()
+        autoCraftConnection = nil
+    end
+    _G.SlowHub.AutoCraftSlime = false
+end
 
-Tab:CreateToggle({
+local function startAutoCraft()
+    if autoCraftConnection then
+        stopAutoCraft()
+    end
+
+    _G.SlowHub.AutoCraftSlime = true
+
+    autoCraftConnection = RunService.Heartbeat:Connect(function()
+        if not _G.SlowHub.AutoCraftSlime then
+            stopAutoCraft()
+            return
+        end
+
+        if tick() - lastCraftTime >= 2 then
+            lastCraftTime = tick()
+            pcall(function()
+                local args = {
+                    [1] = "SlimeKey",
+                }
+                ReplicatedStorage.Remotes.RequestSlimeCraft:InvokeServer(unpack(args))
+            end)
+        end
+    end)
+end
+
+local Toggle = _G.MiscTab:CreateToggle({
     Name = "Auto Craft Slime Key",
-    CurrentValue = false,
+    CurrentValue = _G.SlowHub.AutoCraftSlime or false,
     Flag = "AutoCraftSlimeKey",
     Callback = function(Value)
-        _G.SlowHub.AutoCraftSlime = Value
-        
         if Value then
-            task.spawn(function()
-                while _G.SlowHub.AutoCraftSlime do
-                    pcall(function()
-                        local args = {
-                            [1] = "SlimeKey",
-                        }
-                        ReplicatedStorage.Remotes.RequestSlimeCraft:InvokeServer(unpack(args))
-                    end)
-                    
-                    task.wait(2)
-                end
-            end)
+            startAutoCraft()
+        else
+            stopAutoCraft()
+        end
+        
+        _G.SlowHub.AutoCraftSlime = Value
+        if _G.SaveConfig then
+            _G.SaveConfig()
         end
     end
 })
+
+if _G.SlowHub.AutoCraftSlime then
+    task.wait(2)
+    startAutoCraft()
+end
