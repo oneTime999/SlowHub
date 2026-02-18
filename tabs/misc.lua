79bc2cc5b9470138521a56f360ae595371fa1384
oneTime999 <onetime.zkk@gmail.com>
2026-02-18 17:03:11 -0300

    Update misc.lua

--- tabs/misc.lua
+++ tabs/misc.lua
@@ -1,7 +1,6 @@
 local Tab = _G.MiscTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
--- Hakis Section
 Tab:CreateLabel("Hakis and Ascend")
 
 loadstring(game:HttpGet(githubBase .. "AutoHaki.lua"))()
@@ -9,19 +8,16 @@ loadstring(game:HttpGet(githubBase .. "AutoObservation.lua"))()
 loadstring(game:HttpGet(githubBase .. "AutoConq.lua"))()
 loadstring(game:HttpGet(githubBase .. "AutoAscend.lua"))()
 
--- Auto Skill Section
 Tab:CreateLabel("Auto Skill and Key Craft")
 
 loadstring(game:HttpGet(githubBase .. "AutoSkill.lua"))()
 loadstring(game:HttpGet(githubBase .. "SlimeKey.lua"))()
 loadstring(game:HttpGet(githubBase .. "DivineGrail.lua"))()
 
--- Codes Section
 Tab:CreateLabel("Codes")
 
 loadstring(game:HttpGet(githubBase .. "Codes.lua"))()
 
--- Config Section
 Tab:CreateLabel("Config")
 
 loadstring(game:HttpGet(githubBase .. "AntiAFK.lua"))()
