16192e384a00176b529d4e5923ddfbab19a8f015
oneTime999 <onetime.zkk@gmail.com>
2026-02-21 01:45:14 -0300

    Update Merchant.lua

--- functions/Merchant.lua
+++ functions/Merchant.lua
@@ -19,26 +19,35 @@ local MerchantItemsList = {
 _G.SlowHub.AutoBuyMerchant = false
 _G.SlowHub.SelectedMerchantItems = {}
 
-task.spawn(function()
-    while true do
-        if _G.SlowHub.AutoBuyMerchant then
+local merchantLoopRunning = false
+
+local function startMerchantLoop()
+    if merchantLoopRunning then return end
+    merchantLoopRunning = true
+
+    task.spawn(function()
+        while merchantLoopRunning and _G.SlowHub.AutoBuyMerchant do
             if #_G.SlowHub.SelectedMerchantItems > 0 then
                 for _, itemName in pairs(_G.SlowHub.SelectedMerchantItems) do
                     if not _G.SlowHub.AutoBuyMerchant then break end
-                    
                     pcall(function()
                         ReplicatedStorage.Remotes.MerchantRemotes.PurchaseMerchantItem:InvokeServer(itemName)
                     end)
-                    
-                    task.wait(0)
+                    task.wait(0.5) -- Throttle entre cada compra
                 end
             end
+            task.wait(1) -- Delay entre cada ciclo completo
         end
-        task.wait(0)
-    end
-end)
+        merchantLoopRunning = false
+    end)
+end
 
-local Dropdown = Tab:CreateDropdown({
+local function stopMerchantLoop()
+    merchantLoopRunning = false
+    _G.SlowHub.AutoBuyMerchant = false
+end
+
+Tab:CreateDropdown({
     Name = "Select Merchant Items to Buy",
     Options = MerchantItemsList,
     CurrentOption = {"Select Items"},
@@ -49,20 +58,27 @@ local Dropdown = Tab:CreateDropdown({
     end
 })
 
-local Toggle = Tab:CreateToggle({
+Tab:CreateToggle({
     Name = "Auto Buy Selected Items",
     CurrentValue = false,
     Flag = "AutoBuyMerchant",
     Callback = function(Value)
         _G.SlowHub.AutoBuyMerchant = Value
-        
-        if Value and #_G.SlowHub.SelectedMerchantItems == 0 then
-            _G.Rayfield:Notify({
-                Title = "Warning",
-                Content = "Select at least one item first!",
-                Duration = 3,
-                Image = 4483362458
-            })
+
+        if Value then
+            if #_G.SlowHub.SelectedMerchantItems == 0 then
+                _G.Rayfield:Notify({
+                    Title = "Warning",
+                    Content = "Select at least one item first!",
+                    Duration = 3,
+                    Image = 4483362458
+                })
+                _G.SlowHub.AutoBuyMerchant = false
+                return
+            end
+            startMerchantLoop()
+        else
+            stopMerchantLoop()
         end
     end
 })
