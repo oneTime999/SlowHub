446cc9c3a3c37a37684db1aea36162cc0c7e34b8
oneTime999 <onetime.zkk@gmail.com>
2026-02-21 16:06:58 -0300

    Create teleports.lua

---
+++ tabs/teleports.lua
@@ -0,0 +1,14 @@
+local Tab = _G.TeleportsTab
+local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
+
+if not _G.SlowHub then
+    repeat task.wait(0.1) until _G.SlowHub
+end
+
+Tab:CreateLabel("Teleports")
+
+local teleportScript = game:HttpGet(githubBase .. "Teleports.lua")
+if teleportScript and teleportScript ~= "" then
+    local func = loadstring(teleportScript)
+    if func then pcall(func) end
+end
