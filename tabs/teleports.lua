d857b636137e8f45f3379d13e4713f320ca5daa7
oneTime999 <onetime.zkk@gmail.com>
2026-03-05 10:46:15 -0300

    Update teleports.lua

--- tabs/teleports.lua
+++ tabs/teleports.lua
@@ -5,8 +5,6 @@ if not _G.SlowHub then
     repeat task.wait(0.1) until _G.SlowHub
 end
 
-Tab:CreateLabel("Teleports")
-
 local teleportScript = game:HttpGet(githubBase .. "IslandsTeleport.lua")
 if teleportScript and teleportScript ~= "" then
     local func = loadstring(teleportScript)
