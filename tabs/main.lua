f244005eff2970174a1665bf3dbcd0990f9b3786
oneTime999 <onetime.zkk@gmail.com>
2026-03-05 10:45:53 -0300

    Update main.lua

--- tabs/main.lua
+++ tabs/main.lua
@@ -1,8 +1,6 @@
 local Tab = _G.MainTab
 local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
 
-Tab:CreateLabel("Settings")
-
 local weaponScript = game:HttpGet(githubBase .. "WeaponSelector.lua")
 if weaponScript and weaponScript ~= "" then
     local func = loadstring(weaponScript)
@@ -10,8 +8,6 @@ if weaponScript and weaponScript ~= "" then
 end
 
 task.wait(0.1)
-
-Tab:CreateLabel("Auto Farm")
 
 local levelScript = game:HttpGet(githubBase .. "AutoLevel.lua")
 if levelScript and levelScript ~= "" then
@@ -23,8 +19,6 @@ end
 
 task.wait(0.2)
 
-Tab:CreateLabel("Auto Farm Selected")
-
 local mobScript = game:HttpGet(githubBase .. "AutoFarmSelectedMob.lua")
 if mobScript and mobScript ~= "" then
     local func = loadstring(mobScript)
@@ -34,8 +28,6 @@ if mobScript and mobScript ~= "" then
 end
 
 task.wait(0.2)
-
-Tab:CreateLabel("Auto Dungeon or Boss Rush")
 
 local dungeonScript = game:HttpGet(githubBase .. "AutoDungeon.lua")
 if dungeonScript and dungeonScript ~= "" then
