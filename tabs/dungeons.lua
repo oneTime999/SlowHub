0fc2920b607a349ddaaabd263b4398035c34ca47
oneTime999 <onetime.zkk@gmail.com>
2026-03-27 16:43:10 -0300

    Create dungeons.lua

---
+++ tabs/dungeons.lua
@@ -0,0 +1,13 @@
+-- Dungeons Tab
+local Tab = _G.DungeonsTab
+local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
+
+if not _G.SlowHub then
+    repeat task.wait(0.1) until _G.SlowHub
+end
+
+local autoDungeonScript = game:HttpGet(githubBase .. "AutoDungeon.lua")
+if autoDungeonScript and autoDungeonScript ~= "" then
+    local func = loadstring(autoDungeonScript)
+    if func then task.spawn(function() pcall(func) end) end
+end
