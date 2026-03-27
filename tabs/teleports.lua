12ee9c9f140c0c2e1ec8d48c74b7a0d7b6239686
oneTime999 <onetime.zkk@gmail.com>
2026-03-27 18:05:34 -0300

    Update teleports.lua

--- tabs/teleports.lua
+++ tabs/teleports.lua
@@ -6,14 +6,6 @@ if not _G.SlowHub then
     repeat task.wait(0.1) until _G.SlowHub
 end
 
-local teleportScript = game:HttpGet(githubBase .. "IslandsTeleport.lua")
-if teleportScript and teleportScript ~= "" then
-    local func = loadstring(teleportScript)
-    if func then pcall(func) end
-end
-
-task.wait(0.1)
-
 local npcScript = game:HttpGet(githubBase .. "NPCsTeleport.lua")
 if npcScript and npcScript ~= "" then
     local func = loadstring(npcScript)
