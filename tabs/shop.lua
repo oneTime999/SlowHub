d45eb935ce4ab17d6aa05980cfb27c5404497e8a
oneTime999 <onetime.zkk@gmail.com>
2026-03-27 17:08:15 -0300

    Create shop.lua

---
+++ tabs/shop.lua
@@ -0,0 +1,13 @@
+-- Shop Tab
+local Tab = _G.ShopTab
+local githubBase = "https://raw.githubusercontent.com/oneTime999/SlowHub/main/functions/"
+
+if not _G.SlowHub then
+    repeat task.wait(0.1) until _G.SlowHub
+end
+
+local merchantScript = game:HttpGet(githubBase .. "Merchant.lua")
+if merchantScript and merchantScript ~= "" then
+    local func = loadstring(merchantScript)
+    if func then pcall(func) end
+end
